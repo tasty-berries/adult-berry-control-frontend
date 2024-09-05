f07d3077d463c68dc8c3913e948c2791ac91ebb9
ilya.ignatev <ilya.ignatev@onlinesup.com>
2024-09-05 09:08:30 +0300

    Probably base url fix

--- middleware/authenticated.global.ts
+++ middleware/authenticated.global.ts
@@ -1,11 +1,12 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-    const token = useCookie<string>('token');
+    const token  = useCookie<string>('token');
+    const config = useRuntimeConfig();
 
-    if (!token.value && to.path !== '/login') {
+    if (!token.value && to.path !== config.app.baseURL + '/login') {
         return navigateTo('/login');
     }
 
-    if (token.value && to.path === '/login') {
+    if (token.value && to.path === config.app.baseURL + '/login') {
         return navigateTo('/');
     }
 })
